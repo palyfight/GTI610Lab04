84905d4939791ca2d12d55cb342da7f98cd669a6
cbamatembera <chrisbama92@hotmail.ca>
2015-03-08 20:00:25 -0400

    Started part 2

--- GTI610-Laboratoire04/src/Partie1/Client.java
+++ GTI610-Laboratoire04/src/Partie1/Client.java
@@ -1,53 +1,54 @@
 package Partie1;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
 
 	public static void main(String[] args) {
-		Socket sock = new Socket();
-		String text = "";
+		Socket socket = new Socket();
 		Scanner keyboard = new Scanner(System.in);
-		BufferedWriter bw;
-		//PrintWriter pw;
-		OutputStream os = null;
-		
+
 		try {
-			sock = new Socket("localhost", 8000);
-			os = sock.getOutputStream();
+			socket = new Socket("localhost", 2015);
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		if(sock.isConnected()){
-			while(true){
-				/*System.out.println("Entrez du texte: ");
-				keyboard = new Scanner(System.in);
-				text = keyboard.nextLine();
-				System.out.println("\n");*/
-				bw = new BufferedWriter(new OutputStreamWriter(os));
-				try {
-					//os.write("HELLO WORLD!!!".getBytes());
-					bw.write("Hello World!!");
-					/*os.write(28);
-					os.flush();*/
-					bw.flush();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			} //while
-		} //if
-		
-	}
 
+		while (socket.isConnected()) {
+
+			try {
+				PrintWriter out = new PrintWriter(socket.getOutputStream(),
+						true);
+				String ligne = "Hello World!";
+
+				System.out.println("Quoi? :");
+				ligne = keyboard.nextLine();
+				
+				while (!(ligne.trim().equals("ciao")))
+					out.println(ligne);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			} finally {
+				try {
+					
+					socket.shutdownOutput();
+					socket.close();
+				} catch (IOException e) {
+					
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
+
+		} // if
+	}
 }
--- GTI610-Laboratoire04/src/Partie1/ServeurSocket.java
+++ GTI610-Laboratoire04/src/Partie1/ServeurSocket.java
@@ -1,66 +1,93 @@
 package Partie1;
+
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 
 public class ServeurSocket {
 
 	public static void main(String[] args) {
 		ServerSocket serveur = null;
-		byte[] buff = new byte[256];
-		
-		
-		try {
-			serveur = new ServerSocket(8000);
-		
-		} catch(IOException e) {
-			
-			System.out.print("\n"+e);
-			System.exit(1);
-		}
-
-		boolean estActif = true;
-	    Socket socket = null;
-	    BufferedReader in = null;
-	    String ligne = null;
-	    InputStream ins = null;
-
-		while(estActif){
-			
-		    try {
-		    	socket = serveur.accept();		    	
-		    	System.out.println("Connected!");
-		    	ins = socket.getInputStream();
-		    	ins.read(buff);
-		    	System.out.println(buff[0]);
-				/*in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				
-				while ((ligne = in.readLine()) != null)
-			    {
-					if(ligne.equals("STOP!!")) estActif = false;
-			        System.out.print(ligne + "\n");
-			    }*/
-
-				
-			} catch (IOException e) {
-
-				System.out.print("\n"+e);
-				System.exit(1);
-			}
-		}
 
 		try {
-			socket.close();
-			serveur.close();
-			
+			serveur = new ServerSocket(2015);
+
 		} catch (IOException e) {
-			
-			System.out.print("\n"+e);
+
+			System.out.print("\n" + e);
 			System.exit(1);
+		}
+
+		Socket socket = null;
+		BufferedReader in = null;
+		//String ligne = null;
+
+		while (true) {
+
+			try {
+				socket = serveur.accept();
+				System.out.println("Connection ouverte");
+				
+				String ip = socket.getInetAddress().toString();
+				int port = socket.getPort();
+
+				in = new BufferedReader(new InputStreamReader(
+						socket.getInputStream()));
+
+				/*while ((ligne = in.readLine()) != null) {
+					
+					System.out.println("Reçu: " + ligne);
+				}*/
+				
+				MyThread thread = new MyThread(in, ip, port);
+				thread.start();
+					
+				System.out.println("Connection fermée");
+
+			} catch (IOException e) {
+				if (!socket.isClosed()) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+			} /*finally {
+				try {					
+					socket.shutdownInput();
+					socket.close();
+				} catch (IOException e) {
+
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}*/
+
+		}
+	}
+	
+	public static class MyThread extends Thread{
+		BufferedReader br;
+		String ipAddress;
+		int portNum;
+		
+		MyThread(BufferedReader b, String ip, int port){
+			this.br = b;
+			this.ipAddress = ip;
+			this.portNum = port;
+		}
+		
+		public void run(){
+			/*String ligne = "";
+
+			try {
+				while ((ligne = br.readLine()) != null) {
+					System.out.println("From: " + sock.getInetAddress() +":" + sock.getPort() + "\nMessage: " + ligne);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/
+			System.out.println("THIS IS A TEST NIGGUH! " + ipAddress + ":" + portNum);
 		}
 	}
 }
